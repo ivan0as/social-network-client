36be4e6e99c413ede10869ee8c6ab66413f205b3
ivan0as <83854853+ivan0as@users.noreply.github.com>
2025-08-14 19:05:16 +0700

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,11 @@
 const NAMESITE = 'Сайт'
 
-const URL_IMG = 'https://social-network-server-production.up.railway.app/'
+//const URL_IMG = 'https://social-network-server-production.up.railway.app/'
+
+const URL_IMG = 'http://localhost:5000/'
 
 export {
     NAMESITE,
     URL_IMG,
+
 }
