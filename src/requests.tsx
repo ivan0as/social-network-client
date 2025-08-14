652604c769f756e4df88ec34ae8eb2628689655a
ivan0as <83854853+ivan0as@users.noreply.github.com>
2025-08-14 19:05:41 +0700

    Update requests.tsx

--- src/requests.tsx
+++ src/requests.tsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const request =(option) => {
-    option.url = 'https://social-network-server-production.up.railway.app/api/' + option.url
+    option.url = 'http://localhost:5000/api/' + option.url
+    // option.url = 'https://social-network-server-production.up.railway.app/api/' + option.url
     return axios(option)
    .then(response => {
         return response.data
     })
 }
+
 export { request }
